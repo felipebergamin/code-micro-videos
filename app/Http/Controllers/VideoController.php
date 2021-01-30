0231975fff76a8de7035f85253e6f1864c27169e
Felipe Bergamin <felipebergamin6@gmail.com>
2021-01-30 19:17:56 -0300

    update controllers to extends BasicCrudController

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,84 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
-use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    //
-  }
+  private $rules;
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
+  public function __construct()
   {
-    //
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-    //
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Models\Video  $video
-   * @return \Illuminate\Http\Response
-   */
-  public function show(Video $video)
-  {
-    //
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  \App\Models\Video  $video
-   * @return \Illuminate\Http\Response
-   */
-  public function edit(Video $video)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\Models\Video  $video
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, Video $video)
-  {
-    //
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Models\Video  $video
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(Video $video)
-  {
-    //
+    $this->rules = [
+      'title' => 'required|max:255',
+      'description' => 'required',
+      'year_launched' => 'required|date_format:Y',
+      'opened' => 'boolean',
+      'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
+      'duration' => 'required|integer'
+    ];
   }
 
   protected function model()
@@ -88,15 +25,13 @@ class VideoController extends BasicCrudController
     return Video::class;
   }
 
-  protected function routeStore()
-  {
-  }
-
-  protected function routeUpdate()
-  {
-  }
-
   protected function rulesStore()
   {
+    return $this->rules;
+  }
+
+  protected function rulesUpdate()
+  {
+    return $this->rules;
   }
 }

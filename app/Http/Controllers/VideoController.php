5b10a3e076c300b888cfed21296a7565010c60aa
Felipe Bergamin <felipebergamin6@gmail.com>
2021-01-30 23:31:59 -0300

    feat(video): overwrite controller methods to support relationships

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
+use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
 {
@@ -16,8 +17,32 @@ class VideoController extends BasicCrudController
       'year_launched' => 'required|date_format:Y',
       'opened' => 'boolean',
       'rating' => 'required|in:' . implode(',', Video::RATING_LIST),
-      'duration' => 'required|integer'
+      'duration' => 'required|integer',
+      'categories_id' => 'required|array|exists:categories,id',
+      'genres_id' => 'required|array|exists:genres,id'
     ];
+  }
+
+  public function store(Request $request)
+  {
+    $validData = $this->validate($request, $this->rulesStore());
+    /** @var Video */
+    $obj = $this->model()::create($validData);
+    $obj->categories()->sync($request->get('categories_id'));
+    $obj->genres()->sync($request->get('genres_id'));
+    $obj->refresh();
+    return $obj;
+  }
+
+  public function update(Request $request, $id)
+  {
+    /** @var Video */
+    $obj = $this->findOrFail($id);
+    $validData = $this->validate($request, $this->rulesUpdate());
+    $obj->update($validData);
+    $obj->categories()->sync($request->get('categories_id'));
+    $obj->genres()->sync($request->get('genres_id'));
+    return $obj;
   }
 
   protected function model()

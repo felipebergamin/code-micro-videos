513af13062cfc242caf9b19cf5cb57fb6df506e3
Felipe Bergamin <felipebergamin6@gmail.com>
2021-01-31 11:47:42 -0300

    feat(video): implement transaction for store and update

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Video;
+use Exception;
 use Illuminate\Http\Request;
 
 class VideoController extends BasicCrudController
@@ -26,12 +27,15 @@ class VideoController extends BasicCrudController
   public function store(Request $request)
   {
     $validData = $this->validate($request, $this->rulesStore());
-    /** @var Video */
-    $obj = $this->model()::create($validData);
-    $obj->categories()->sync($request->get('categories_id'));
-    $obj->genres()->sync($request->get('genres_id'));
-    $obj->refresh();
-    return $obj;
+    $self = $this;
+    $video = \DB::transaction(function () use ($request, $validData, $self) {
+      /** @var Video */
+      $obj = $this->model()::create($validData);
+      $self->handleRelations($obj, $request);
+      return $obj;
+    });
+    $video->refresh();
+    return $video;
   }
 
   public function update(Request $request, $id)
@@ -39,10 +43,20 @@ class VideoController extends BasicCrudController
     /** @var Video */
     $obj = $this->findOrFail($id);
     $validData = $this->validate($request, $this->rulesUpdate());
-    $obj->update($validData);
-    $obj->categories()->sync($request->get('categories_id'));
-    $obj->genres()->sync($request->get('genres_id'));
-    return $obj;
+
+    $self = $this;
+    $video = \DB::transaction(function () use ($request, $validData, $obj, $self) {
+      $obj->update($validData);
+      $self->handleRelations($obj, $request);
+      return $obj;
+    });
+    return $video;
+  }
+
+  protected function handleRelations($video, Request $request)
+  {
+    $video->categories()->sync($request->get('categories_id'));
+    $video->genres()->sync($request->get('genres_id'));
   }
 
   protected function model()

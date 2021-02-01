75db61f89412e55a0b6deafbec3518e6c2accba2
Felipe Bergamin <felipebergamin6@gmail.com>
2021-02-01 11:09:24 -0300

    tests(cast_member): add unit and integration tests for CastMember

---
+++ tests/Unit/CastMemberUnitTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\CastMember;
+use App\Models\Traits\Uuid;
+use PHPUnit\Framework\TestCase;
+
+class CastMemberUnitTest extends TestCase
+{
+  /** @var CastMember */
+  private $castMember;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+    $this->castMember = new CastMember();
+  }
+
+  public function testFillable()
+  {
+    $fillable = ['name', 'type'];
+    $this->assertEquals(
+      $fillable,
+      $this->castMember->getFillable(),
+    );
+  }
+
+  public function testIfUseTraits()
+  {
+    $traits = [
+      HasFactory::class,
+      SoftDeletes::class,
+      Uuid::class,
+    ];
+    $castMemberTraits = array_keys(class_uses(CastMember::class));
+    ($castMemberTraits);
+    $this->assertEquals($traits, $castMemberTraits);
+  }
+
+  public function testDatesAttribute()
+  {
+    $dates = ['deleted_at', 'updated_at', 'created_at'];
+    $this->assertEqualsCanonicalizing($dates, $this->castMember->getDates());
+    $this->assertCount(count($dates), $this->castMember->getDates());
+  }
+
+  public function testCastsAttribute()
+  {
+    $casts = ['id' => 'string', 'deleted_at' => 'datetime'];
+    $this->assertEqualsCanonicalizing($casts, $this->castMember->getCasts());
+  }
+
+  public function testIncrementing()
+  {
+    $this->assertEquals($this->castMember->incrementing, false);
+  }
+}

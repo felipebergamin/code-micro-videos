7880b11ecbbeca9a615798e2cb101f213683b6dd
Felipe Bergamin <felipebergamin6@gmail.com>
2021-02-07 20:10:48 -0300

    feat(video_model): overwrite create and update methods

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use \DB;
 
 class Video extends Model
 {
@@ -31,6 +32,42 @@ class Video extends Model
   ];
 
   public $incrementing = false;
+
+  public static function create(array $attributes = [])
+  {
+    try {
+      DB::beginTransaction();
+      $obj = static::query()->create($attributes);
+      // TODO: handle the uploads here
+      DB::commit();
+      return $obj;
+    } catch (\Exception $e) {
+      if (isset($obj)) {
+        // TODO: delete uploaded files
+      }
+      DB::rollBack();
+      throw $e;
+    }
+  }
+
+  public function update(array $attributes = [], array $options = [])
+  {
+    try {
+      DB::beginTransaction();
+      $saved = parent::update($attributes, $options);
+      if ($saved) {
+        // upload
+        // delete old files
+      }
+      // TODO: handle the uploads here
+      DB::commit();
+      return $saved;
+    } catch (\Exception $e) {
+      // TODO: delete uploaded files
+      DB::rollBack();
+      throw $e;
+    }
+  }
 
   public function categories()
   {

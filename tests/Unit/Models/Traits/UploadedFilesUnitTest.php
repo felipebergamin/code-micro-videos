e439a9e1b2addce079c101ed6e71b07cdacfc1b8
Felipe Bergamin <felipebergamin6@gmail.com>
2021-03-09 20:46:05 -0300

    test(upload): test extract files function

--- tests/Unit/Models/Traits/UploadedFilesUnitTest.php
+++ tests/Unit/Models/Traits/UploadedFilesUnitTest.php
@@ -51,4 +51,38 @@ class UploadedFilesUnitTest extends TestCase
     \Storage::assertMissing("1/{$file1->hashName()}");
     \Storage::assertMissing("1/{$file2->hashName()}");
   }
+
+  public function testExtractFiles()
+  {
+    $attributes = [];
+    $files = UploadFilesStub::extractFiles($attributes);
+    $this->assertCount(0, $attributes);
+    $this->assertCount(0, $files);
+
+    $attributes = ['file1' => 'teste'];
+    $files = UploadFilesStub::extractFiles($attributes);
+    $this->assertEquals(['file1' => 'teste'], $attributes);
+    $this->assertCount(1, $attributes);
+    $this->assertCount(0, $files);
+
+    $attributes = ['file1' => 'teste', 'file2' => 'teste'];
+    $files = UploadFilesStub::extractFiles($attributes);
+    $this->assertEquals(['file1' => 'teste', 'file2' => 'teste'], $attributes);
+    $this->assertCount(2, $attributes);
+    $this->assertCount(0, $files);
+
+    $file1 = UploadedFile::fake()->create('video1.mp4');
+    $attributes = ['file1' => $file1, 'other' => 'test'];
+    $files = UploadFilesStub::extractFiles($attributes);
+    $this->assertEquals(['file1' => $file1->hashName(), 'other' => 'test'], $attributes);
+    $this->assertCount(2, $attributes);
+    $this->assertEquals([$file1], $files);
+
+    $file2 = UploadedFile::fake()->create('video2.mp4');
+    $attributes = ['file1' => $file1, 'file2' => $file2, 'other' => 'test'];
+    $files = UploadFilesStub::extractFiles($attributes);
+    $this->assertEquals(['file1' => $file1->hashName(), 'file2' => $file2->hashName(), 'other' => 'test'], $attributes);
+    $this->assertCount(3, $attributes);
+    $this->assertEquals([$file1, $file2], $files);
+  }
 }

6816e6d97761c7d3c7216dd1e6faf896b4098396
Felipe Bergamin <felipebergamin6@gmail.com>
2021-03-30 18:47:05 -0300

    chore: update docker files according to new repo structure

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+
+# front-end
+cd /var/www/frontend && npm install && cd ..
+
+# back-end
 cd backend
 if [ ! -f .env ]; then
   cp .env.example .env

4461fc83c851d06a0cd9e71bfd978f713df632c4
Felipe Bergamin <felipebergamin6@gmail.com>
2021-03-31 22:04:09 -0300

    feat: create basic page structure using material-ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
+import { Box } from '@material-ui/core';
 
-import { Button } from '@material-ui/core';
+import Navbar from './components/Navbar';
+import Page from './components/Page';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button color="primary" variant="contained">
-            Texto
-          </Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Box paddingTop="80px">
+        <Page title="Category">
+          Conteúdo
+        </Page>
+      </Box>
+    </>
   );
 }
 

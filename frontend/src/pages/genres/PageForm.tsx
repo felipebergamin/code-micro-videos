90a92df8e870ab39712682736a740e97cb65090e
Felipe Bergamin <felipebergamin6@gmail.com>
2021-05-29 17:56:54 -0300

    feat(genres): add genre form
    Create genre form component, able to create new genres on back-end.
    Add it to navigation routes.

---
+++ frontend/src/pages/genres/PageForm.tsx
@@ -0,0 +1,12 @@
+import Page from '../../components/Page';
+import Form from './Form';
+
+const PageForm = (): JSX.Element => {
+  return (
+    <Page title="Criar gênero">
+      <Form />
+    </Page>
+  );
+};
+
+export default PageForm;

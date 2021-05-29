177a757eeef8061818c0d98741b76d071cb3da3c
Felipe Bergamin <felipebergamin6@gmail.com>
2021-05-29 16:20:19 -0300

    feat(cast_member): add Form and PageForm components
    Cast member form can create new cast members

---
+++ frontend/src/pages/cast_members/PageForm.tsx
@@ -0,0 +1,12 @@
+import Page from '../../components/Page';
+import Form from './Form';
+
+const PageForm = (): JSX.Element => {
+  return (
+    <Page title="Criar membro de elenco">
+      <Form />
+    </Page>
+  );
+};
+
+export default PageForm;
